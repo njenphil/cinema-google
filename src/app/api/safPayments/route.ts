29c1044ebe422d5aa999cddc10e47ff14d8fd645
unknown <advertronics@gmail.com>
2024-07-09 18:01:45 +0300

    fixed business short code

--- src/app/api/safPayments/route.ts
+++ src/app/api/safPayments/route.ts
@@ -34,13 +34,13 @@ export async function POST(req: Request) {
       method: "POST",
       headers,
       body: JSON.stringify({
-        BusinessShortCode: 5641457, // Store Number
+        BusinessShortCode: 174379, // Store Number
         Password: Password,
         Timestamp: Timestamp,
         TransactionType: "CustomerPayBillOnline", //"CustomerBuyGoodsOnline"
         Amount: 1,
         PartyA: 254708374149,
-        PartyB: 7609299, //  Till Number
+        PartyB: 174379, //  Till Number
         PhoneNumber: `254${PhoneNumber}`,
         CallBackURL: callBackURL,
         AccountReference: AccountReference,
